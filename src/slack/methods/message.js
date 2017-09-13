6cf272f806349dbb99806f6a1ce67e3c41a90d02
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2017-09-13 18:57:09 +0700

    feat: framework for additional methods

---
+++ src/slack/methods/message.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const avow = require('avow')
+const {
+  first,
+} = require('lodash/fp')
+
+/**
+ * Returns a Message object, given a Slack Channel ID and a timestamp.
+ *
+ * @method getMessage
+ * @param {Object} slack - The wrapped slack
+ * @param {String} options.channelId - The Slack Channel ID like 'C012345'
+ * @param {String} options.ts - The unix timestamp with microsecond precision
+ *   (like '1499718817.760039'). This is the format returned by Slack's API
+ * @return {Object} - A Slack Message object as returned by the API, or throws if
+ *   the channel is not found, or the ts is invalid
+ */
+const getMessage = slack =>
+  async ({ channelId, ts }) => {
+    avow(ts, 'no ts given')
+    const response = await slack.channels.history({
+      channel: channelId,
+      inclusive: true,
+      latest: ts,
+      oldest: ts,
+      count: 1,
+    })
+    return first(response.messages)
+  }
+
+module.exports = {
+  getMessage,
+}
---
+++ test/specs/slack/methods/message.spec.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const avow = require('avow')
+const sinon = require('sinon')
+const chance = require('@invisible/chance-extras')
+const {
+  find,
+  get,
+  keys,
+  map,
+} = require('lodash/fp')
+
+const channelsHistoryFixture = require('~/test/fixtures/channels.history.json')
+const channelsHistoryFixture2 = require('~/test/fixtures/channels.history2.json')
+const { getMessage } = require('~/src/slack/methods/message')
+
+describe('slack/methods/message', () => {
+  describe('getMessage', () => {
+    const setup = () => {
+      const channels = {
+        [chance.slackChannelId()]: channelsHistoryFixture,
+        [chance.slackChannelId()]: channelsHistoryFixture2,
+      }
+
+      const historyStub = sinon.stub().callsFake(async ({ channel, latest }) => {
+        const fixture = channels[channel]
+        const messages = get('messages')(fixture)
+        const message = find({ ts: latest })(messages)
+        return { messages: [ message ] }
+      })
+
+      const slack = {
+        channels: {
+          history: historyStub,
+        },
+      }
+      return {
+        channels,
+        slack,
+      }
+    }
+
+    it('should get the correct message', async () => {
+      const { channels, slack } = setup()
+      const channelId = chance.pickone(keys(channels))
+      const fixture = channels[channelId]
+      const messages = get('messages')(fixture)
+      const ts = chance.pickone(map('ts')(messages))
+
+      const expected = find({ ts })(messages)
+      const actual = await getMessage(slack)({ channelId, ts })
+
+      avow.deepStrictEqual(expected, actual)
+    })
+  })
+})
