6db2195cc871c5c8b0d1df4b07015abad14e8beb
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2017-09-13 17:57:34 +0700

    feat: utility helpers

---
+++ test/specs/helpers/utility.spec.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const avow = require('avow')
+
+const {
+  isTestEnv,
+  exportForTest,
+} = require('~/src/helpers/utility')
+
+describe('helpers/utility', () => {
+  afterEach(() => {
+    process.env.NODE_ENV = 'test'
+  })
+
+  describe('isTestEnv', () => {
+    it('should return true if in test env', async () => {
+      process.env.NODE_ENV = 'test'
+      avow(isTestEnv())
+    })
+
+    it('should return false if not in test env', async () => {
+      process.env.NODE_ENV = 'development'
+      avow(! isTestEnv())
+    })
+  })
+
+  describe('exportForTest', () => {
+    const setup = () => {
+      const stuff = 'hi'
+      const stuff2 = 'thing'
+
+      const theModule = {}
+
+      theModule.exports = {
+        stuff,
+      }
+      return { stuff, stuff2, theModule }
+    }
+
+    it('should merge given functions to module.exports when in test env', async () => {
+      const { stuff, stuff2, theModule } = setup()
+
+      process.env.NODE_ENV = 'test'
+      exportForTest(theModule, { stuff2 })
+
+      avow.deepStrictEqual(theModule.exports, { stuff, stuff2 })
+    })
+
+    it('should ignore extra exports when not in test env', async () => {
+      const { stuff, stuff2, theModule } = setup()
+
+      process.env.NODE_ENV = 'development'
+      exportForTest(theModule, { stuff2 })
+
+      avow.deepStrictEqual(theModule.exports, { stuff })
+    })
+  })
+})
