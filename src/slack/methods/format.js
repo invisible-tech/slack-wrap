4e83a77286c786a31bbfcdf869ca8fe99f11d694
rodrigo4244 <rodrigo4244@users.noreply.github.com>
2017-10-13 10:01:12 -0300

    fix: remove get from method names

--- src/slack/methods/format.js
+++ src/slack/methods/format.js
@@ -3,26 +3,26 @@
 const { oneLineTrim } = require('common-tags')
 const { replace } = require('lodash/fp')
 
-const getChannelLink = ({ channelId, display }) =>
+const channelLink = ({ channelId, display }) =>
   (display
     ? `<#${channelId}|${display}>`
     : `<#${channelId}>`)
 
 const removeDot = replace('.')('')
-const getMessageLink = ({ teamName, channelId, ts, display }) =>
+const messageLink = ({ teamName, channelId, ts, display }) =>
   (display
     ? oneLineTrim`<https://${teamName}.slack.com/archives/
         ${channelId}/p${removeDot(ts)}|${display}>`
     : oneLineTrim`<https://${teamName}.slack.com/archives/
         ${channelId}/p${removeDot(ts)}>`)
 
-const getUserLink = ({ userId, display }) =>
+const userLink = ({ userId, display }) =>
   (display
     ? `<@${userId}|${display}>`
     : `<@${userId}>`)
 
 module.exports = {
-  getChannelLink,
-  getMessageLink,
-  getUserLink,
+  channelLink,
+  messageLink,
+  userLink,
 }
